711e4cd679c20a5ba4512718ec64edf21a595c58
eduaardo011 <eduardoalmeida01234567@gmail.com>
2026-09-24 14:49:57 -0300

    Add files via upload

---
+++ atividade-função-JS/javascript2.js
@@ -0,0 +1,13 @@
+//Atividade Facil, Não precisei pensar muito em lógica e essas coisas
+
+function calcularAreaRetangulo() {
+
+    let altura = Number(prompt("Digite a altura do Retângulo"))
+    let base = Number(prompt("Digite o tamanho da base do Retângulo"))
+    let area
+
+    area = altura * base;
+
+    alert(`A área do Retângulo é ${area} cm²`)
+}
+calcularAreaRetangulo()
